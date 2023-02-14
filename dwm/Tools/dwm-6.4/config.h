612d5c206e594d6d425fda2ae610a337d471e209
Wojciech M. Wnuk <laniusone@pm.me>
2023-02-14 15:28:08 +0100

    dwm rebuilt with fix

--- dwm/Tools/dwm-6.4/config.h
+++ dwm/Tools/dwm-6.4/config.h
@@ -29,7 +29,7 @@ static const Rule rules[] = {
 	/* class         instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",        NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
-    { "xfce4-panel", NULL,       NULL,       ~0,           1,           -1 },
+    { "Xfce4-panel", NULL,       NULL,       ~0,           1,           -1 },
 };
 
 /* layout(s) */

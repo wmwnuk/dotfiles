a5fd392b2aa07169818a3894ecbbc104c5a3000b
Wojciech M. Wnuk <laniusone@pm.me>
2023-02-14 10:54:12 +0100

    clean build of dwm

--- dwm/Tools/dwm-6.4/config.mk
+++ dwm/Tools/dwm-6.4/config.mk
@@ -4,8 +4,9 @@ VERSION = 6.4
 # Customize below to fit your system
 
 # paths
-PREFIX = /usr/local
+PREFIX = /.local
 MANPREFIX = ${PREFIX}/share/man
+DESTDIR = ~/Projects/dotfiles/dwm
 
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib

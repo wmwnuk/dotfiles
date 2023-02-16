37555af0f53144869708da23a45ed4a493369ab7
Wojciech M. Wnuk <laniusone@pm.me>
2023-02-16 13:32:00 +0100

    i3 workspaces genmon script + genmon styling in gtk.css

--- bin/.local/bin/i3-workspaces.py
+++ bin/.local/bin/i3-workspaces.py
@@ -1,20 +1,45 @@
 #!/usr/bin/env python3
 
-import json, subprocess
+import json, subprocess, sys
 
-workspaces_raw = subprocess.check_output(['i3-msg', '-t', 'get_workspaces'])
+def get_workspaces(mode):
+    workspaces_raw = subprocess.check_output(['i3-msg', '-t', 'get_workspaces'])
+    workspaces = sorted(json.loads(workspaces_raw), key=lambda d: d['name'])
 
-workspaces = json.loads(workspaces_raw);
+    if mode == 'unfocused':
+        return [workspace for workspace in workspaces if workspace['focused'] == False]
+    elif mode != 'all':
+        focused = [workspace for workspace in workspaces if workspace['focused']]
 
-output = '<txt>'
+        if mode == 'focused':
+            return focused
+        elif mode == 'focused-':
+            return [workspace for workspace in workspaces if workspace['num'] < focused[0]['num']]
+        elif mode == 'focused+':
+            return [workspace for workspace in workspaces if workspace['num'] > focused[0]['num']]
 
-for workspace in workspaces:
-    visible = '<span foreground="#565f89">' if workspace['visible'] else '<span>'
-    focused = '<span foreground="#a9b1d6">' if workspace['focused'] else '<span>'
-    urgent = '<span foreground="#f7768e">' if workspace['urgent'] else '<span>'
-    output += '<span foreground="#414868">' + visible + focused + urgent + ' ' \
-            + workspace['name'].split(':')[1] + ' </span></span></span></span>'
+    return workspaces
 
-output += '</txt>'
+def print_workspaces(mode):
+    fg = '<span foreground="#292e42">'
+    output = '<txt>{}</txt>'
+    if mode == 'focused':
+        output += '<txtclick>rofi -modi window -show window</txtclick>'
 
-print(output)
+    workspaces = get_workspaces(mode)
+    workspaces_txt = ''
+
+    for workspace in workspaces:
+        visible = '<span foreground="#565f89">' if workspace['visible'] else '<span>'
+        focused = '<span foreground="#a9b1d6">' if workspace['focused'] else '<span>'
+        urgent = '<span foreground="#f7768e">' if workspace['urgent'] else '<span>'
+        workspaces_txt += fg + visible + focused + urgent + ' ' \
+                + workspace['name'].split(':')[1] + ' </span></span></span></span>'
+
+    print(output.format(workspaces_txt))
+
+if __name__ == '__main__':
+
+    mode = sys.argv[1] if len(sys.argv) > 1 else 'all'
+
+    print_workspaces(mode)

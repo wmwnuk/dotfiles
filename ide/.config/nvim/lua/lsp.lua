009786be663213f2195a7128801b2b10e8ffedf1
Wojciech M. Wnuk <laniusone@pm.me>
2023-03-14 12:37:57 +0100

    remove old nvim config

--- ide/.config/nvim/lua/lsp.lua
+++
@@ -1,6 +0,0 @@
-local defaults = require('sigma.lsp.defaults')
-defaults.init_options = {
-    licenceKey = '/home/lanius/.vim/intelephense/licence.txt',
-    globalStoragePath = os.getenv('HOME') .. '/.local/share/intelephense'
-}
-require("lspconfig")['intelephense'].setup(defaults)

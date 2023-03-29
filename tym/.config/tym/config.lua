acd2976ed3fe06f8219f3354f16bd8b46d40cbc3
Wojciech M. Wnuk <laniusone@pm.me>
2023-03-29 20:39:54 +0200

    tym terminal config

---
+++ tym/.config/tym/config.lua
@@ -0,0 +1,7 @@
+local tym = require('tym')
+
+tym.set('font', 'SauceCodePro Nerd Font 10')
+tym.set('padding_horizontal', 10)
+tym.set('padding_vertical', 10)
+
+
---
+++ tym/.config/tym/theme.lua
@@ -0,0 +1,28 @@
+local bg = '#1a1b26'
+local fg = '#a9b1d6'
+
+return {
+    color_background = bg,
+    color_foreground = fg,
+    color_bold = fg,
+    color_cursor = '#e0af68',
+    color_cursor_foreground = bg,
+    color_highlight = '#33467c',
+    color_highlight_foreground = '#c0caf5',
+    color_0  = bg,
+    color_1  = '#f7768e',
+    color_2  = '#9ece6a',
+    color_3  = '#e0af68',
+    color_4  = '#7aa2f7',
+    color_5  = '#ad8ee6',
+    color_6  = '#73daca',
+    color_7  = '#787c99',
+    color_8  = '#444b6a',
+    color_9  = '#ff7a93',
+    color_10 = '#b9f27c',
+    color_11 = '#ff9e64',
+    color_12 = '#7da6ff',
+    color_13 = '#bb9af7',
+    color_14 = '#0db9d7',
+    color_15 = '#acb0d0',
+}

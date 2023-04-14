598d09ddc5b4949819c28de6a1c50be496556d19
Wojciech M. Wnuk <laniusone@pm.me>
2023-04-14 19:29:02 +0200

    zoxide zix fix

--- zsh/.config/zsh/completions.d/zoxide.zsh
+++ zsh/.config/zsh/completions.d/zoxide.zsh
@@ -2,11 +2,11 @@ eval "$(zoxide init zsh)"
 
 \builtin unalias zix &>/dev/null || \builtin true
 function zix() { 
-    if [[ -z "$@" ]]; then
-        __zoxide_z "$@"
+    if [[ -d "$@" ]]; then
+        __zoxide_z "$@" >/dev/null
     elif [[ -d "$PWD/$@" ]]; then
-        __zoxide_z "$@"
+        __zoxide_z "$@" >/dev/null
     else
-        __zoxide_zi "$@" > /dev/null || __zoxide_z "$@"
+        __zoxide_zi "$@" >/dev/null || __zoxide_z "$@"
     fi
 }

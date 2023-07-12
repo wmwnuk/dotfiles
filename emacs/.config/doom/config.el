865a15601aa15beb8a4e4a8dffded2cae7eea3d0
Wojciech M. Wnuk <laniusone@pm.me>
2023-07-13 01:22:59 +0200

    dmenu in elisp

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -129,6 +129,37 @@
                                               (split-string (shell-command-to-string "grep 'Host ' ~/.ssh/config | cut -b 6- | grep -v '\*'") "\n" t)
                                               nil t))))
 
+(defun emenu/run ()
+  "Run command"
+  (interactive)
+  (call-process-shell-command (completing-read "run: "
+                                               (split-string
+                                                (shell-command-to-string "(for pdir in $(echo \"$PATH\" | tr \":\" \"\\n\")
+do
+\\find \"$pdir\" -maxdepth 1 -executable -type f,l -printf \"%f\\n\" 2>/dev/null
+done)|sort")
+                                                "\n" t)
+                                               nil nil)
+                              nil 0))
+
+(defun emenu/run-frame ()
+  "Yoinked from DT"
+  (interactive)
+  (with-selected-frame
+    (make-frame '((name . "emacs-run-launcher")
+                  (minibuffer . only)
+                  (fullscreen . 0) ; no fullscreen
+                  (undecorated . t) ; remove title bar
+                  ;;(auto-raise . t) ; focus on this frame
+                  ;;(tool-bar-lines . 0)
+                  ;;(menu-bar-lines . 0)
+                  (internal-border-width . 10)
+                  (width . 80)
+                  (height . 11)))
+                  (unwind-protect
+                    (emenu/run)
+                    (delete-frame))))
+
 (use-package! beacon
   :config
   (beacon-mode 1))

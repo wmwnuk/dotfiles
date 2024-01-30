781e0253a3e18189e0fc4c866da6e50b3e6c528e
Wojciech M. Wnuk <laniusone@pm.me>
2024-01-31 00:56:59 +0100

    tgpt integration in Emacs

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -449,3 +449,24 @@ file at point."
 
 (when (getenv "WSLENV")
   (shell-command-to-string "sudo rm -r /tmp/.X11-unix;ln -s /mnt/wslg/.X11-unix /tmp/.X11-unix"))
+
+(defun get-or-create-buffer (buffer-name)
+  (let ((tgpt (get-buffer buffer-name)))
+    (if tgpt
+        tgpt
+      (generate-new-buffer buffer-name))))
+
+(defun tgpt-buffer ()
+  (interactive)
+  (let ((tgpt (get-or-create-buffer "*TGPT*")))
+    (switch-to-buffer tgpt)
+    (funcall 'markdown-mode)))
+
+(defun tgpt-prompt ()
+  (interactive)
+  (let ((prompt (read-string "tgpt: ")))
+    (tgpt-buffer)
+    (insert (format "### %s" prompt))
+    (insert "\n\n")
+    (insert (shell-command-to-string (format "tgpt -q -w '%s' " prompt)))
+    (insert "\n\n")))

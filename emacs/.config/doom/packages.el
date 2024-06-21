7b17f741e335913ec1a09368e80319ca46acd67e
Wojciech M. Wnuk <laniusone@pm.me>
2024-06-21 22:42:58 +0200

    update and pin all custom packages

--- emacs/.config/doom/packages.el
+++ emacs/.config/doom/packages.el
@@ -1,14 +1,15 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
-(package! vimrc-mode)
-(package! ellama)
-(package! prettier-js)
-(package! add-node-modules-path)
-(package! blamer)
+(package! vimrc-mode :pin "13bc150a870d5d4a95f1111e4740e2b22813c30e")
+(package! ellama :pin "3d6192ea09cfbd2e3d3e13dcada5f543bd79b968")
+(package! prettier-js :pin "e9b73e81d3e1642aec682195f127a42dfb0b5774")
+(package! add-node-modules-path :pin "841e93dfed50448da66c89a977c9182bb18796a1")
+(package! blamer :pin "281963da789fe9e47ce9efc4f7a9c4d19bf3a405")
 (package! copilot
-  :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+  :recipe (:host github :repo "copilot-emacs/copilot.el" :files ("*.el" "dist")) :pin "733bff26450255e092c10873580e9abfed8a81b8")
 (package! solaire-mode :disable t)
-(package! obsidian)
-(package! beacon)
+(package! obsidian :pin "5aeb88cec9814be2b0b047453d797a29628b5a91")
+(package! beacon :pin "85261a928ae0ec3b41e639f05291ffd6bf7c231c")
 (package! transient :pin "4f0fe22cafb6c2c8c8749a9037351ed01cf121ef")
+(package! vertico-posframe :pin "6c3d0d900cb6e6554a1552ed93b84b0c4a78d015")
